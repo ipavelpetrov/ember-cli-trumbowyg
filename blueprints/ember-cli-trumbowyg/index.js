52a9708648b663ab5d680bcddf0618ad7ce36b96
Pavel Petrov <ipavelpetrov@ya.ru>
2016-06-10 15:31:41 +0400

    fix blueprints

--- blueprints/ember-cli-trumbowyg/index.js
+++ blueprints/ember-cli-trumbowyg/index.js
@@ -1,5 +1,7 @@
 /*jshint node:true*/
 module.exports = {
+  normalizeEntityName: function() {}, // no-op since we're just adding dependencies
+
   afterInstall: function(options) {
     return this.addBowerPackagesToProject([
         { name: 'trumbowyg', target: '~2.1.0' },
